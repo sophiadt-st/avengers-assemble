72d78f6236a3bde03c653706991bf93c2792a921
sophiadt-st <sophia.detorres@sailthru.com>
2022-08-18 14:11:48 +1200

    [CREATE-SEARCH] Add routing to next page

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,24 +1,47 @@
-import { json } from '@remix-run/node';
+import { json, redirect } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
-import { SearchBox } from '~/components/SearchBox';
 import { getAvengers } from '~/models/avengers.server';
-import { useState } from 'react';
+import { DatalistInput, useComboboxControls } from 'react-datalist-input';
+import { Form } from '@remix-run/react';
 
 export const loader = async () => {
-  return json(await getAvengers());
+    return json(await getAvengers());
+};
+
+export async function action({ request }) {
+    const body = await request.formData();
+    const avengerName = body.get('hero');
+    const avengersId = await findAvengerID(avengerName)
+    return redirect(`/avengers/${avengersId}/details`);
+}
+
+async function findAvengerID(avengerName) {
+  const avengers = await getAvengers()
+  const avengersId = avengers.find(avenger => avenger.value === avengerName).id
+  return avengersId
 }
 
 export default function Index() {
-  const [avenger, setAvenger] = useState(null);
-  const avengers = useLoaderData();
-  return (
-    <main className="container mx-auto">
-      <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <SearchBox avengers={avengers} setAvenger={setAvenger}/>   
-      </div>
-    </main>
-  );
+    const { setAvenger, avenger  } = useComboboxControls({ initialValue: "Black Panther" });
+    const avengers = useLoaderData();
+
+    return (
+        <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+            <div>
+                <Form method="post">
+                    <DatalistInput
+                        inputProps={{
+                          name: "hero"
+                        }}
+                        value={avenger}
+                        setValue={setAvenger}
+                        label="Search Avenger"
+                        items={avengers}
+                    />
+                    <button type="submit">Search</button>
+                </Form>
+            </div>
+        </div>
+    );
 }
