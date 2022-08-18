627d08d982139a83e030c519ac8b24a651b3a336
Colin Greene <colin.greene@colingreene.local>
2022-08-18 16:17:01 +1200

    [STYLING] update-styles

--- app/routes/avengers.jsx
+++ app/routes/avengers.jsx
@@ -2,9 +2,9 @@ import { Outlet } from '@remix-run/react';
 
 export default function Index() {
     return (
-        <main className="container mx-auto">
+        <>
             <h1>Hello from avengers route!</h1>
             <Outlet />
-        </main>
+        </>
     )
 }
--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,8 +1,7 @@
 import { json, redirect } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
+import { Form, useLoaderData } from '@remix-run/react';
 import { getAvengers } from '~/models/avengers.server';
 import { DatalistInput, useComboboxControls } from 'react-datalist-input';
-import { Form } from '@remix-run/react';
 
 export const loader = async () => {
     return json(await getAvengers());
@@ -26,20 +25,29 @@ export default function Index() {
     const avengers = useLoaderData();
 
     return (
-        <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }} className="flex justify-center">
             <div>
-                <Form method="post">
+                <Form method="post" className="flex">
                     <DatalistInput
                         inputProps={{
-                          name: "hero"
+                            name: "hero",
+                            className: "border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full h-10 p-2.5 relative"
+                        }}
+                        listboxProps={{
+                            className: "absolute"
+                        }}
+                        listboxOptionProps={{
+                            className: "p-1 hover:bg-blue-700 hover:text-white"
                         }}
                         value={avenger}
                         setValue={setAvenger}
                         label="Search Avenger"
                         items={avengers}
                     />
-                    <button type="submit">Search</button>
+                    <button type="submit" className="p-2.5 h-10 ml-2 text-sm self-end font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+                        <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                        <span className="sr-only">Search</span>
+                    </button>
                 </Form>
             </div>
         </div>
