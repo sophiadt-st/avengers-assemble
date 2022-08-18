39691580bebb742f48dd95c442ec5d55ecc7e2ba
sophiadt-st <sophia.detorres@sailthru.com>
2022-08-18 16:55:11 +1200

    [STYLE-DETAILS] Add styling to details page

--- app/routes/avengers.jsx
+++ app/routes/avengers.jsx
@@ -3,7 +3,6 @@ import { Outlet } from '@remix-run/react';
 export default function Index() {
     return (
         <>
-            <h1>Hello from avengers route!</h1>
             <Outlet />
         </>
     )
